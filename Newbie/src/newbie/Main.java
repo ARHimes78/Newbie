49697385cca06a89928f4dbfb140f5c224d97ad7
ARHimes78 <splabbity@Alans-MacBook-Pro.local>
2015-03-15 14:11:14 -0600

    first upload
    first upload

---
+++ Newbie/src/newbie/Main.java
@@ -0,0 +1,10 @@
+package newbie;
+
+import javax.swing.JOptionPane;
+
+
+public class Main {
+    public static void main(String[] args) {
+           new JOptionPane().showMessageDialog(null, "Hello, world!", "First GitHub upload", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
