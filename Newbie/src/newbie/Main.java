a1bd2d596ce1d57e4d049b360a74b1d16f6d06c5
ARHimes78 <splabbity@Alans-MacBook-Pro.local>
2015-03-15 14:25:41 -0600

    Line too long

--- Newbie/src/newbie/Main.java
+++ Newbie/src/newbie/Main.java
@@ -5,6 +5,7 @@ import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
-           new JOptionPane().showMessageDialog(null, "Hello, world!", "First GitHub upload", JOptionPane.INFORMATION_MESSAGE);
+        new JOptionPane().showMessageDialog(null, "Hello, world!", 
+            "First GitHub upload", JOptionPane.INFORMATION_MESSAGE);
     }
 }
